4ec5db1fdcc8f400d7f81fcc5e178e551966f9e5
kaziu144 <kaziu144@gmail.com>
2020-04-23 13:42:52 +0200

    homework

--- src/com/Kodilla/Main.java
+++ src/com/Kodilla/Main.java
@@ -3,6 +3,36 @@ package com.Kodilla;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	MobilePhone Alcatel = new MobilePhone("200 gram",300);
+        System.out.println(Alcatel.showPrice());
+        System.out.println(Alcatel.priceMinusTenProcent());
+        System.out.println(Alcatel.pricePlusDefaultValue(50));
     }
+}
+class MobilePhone{
+    String weight;
+    int price;
+    public MobilePhone(String weight, int price){
+        this.weight=weight;
+        this.price=price;
+
+    }
+    public void displayValues(){
+        System.out.println(this.weight+ " "+ this.price);
+    }
+    public  String getParameters(){
+        return this.weight+" "+this.price;
+    }
+    public String showPrice(){
+        return "Cena telefonu to "+this.price;
+    }
+    public String priceMinusTenProcent(){
+        double result =this.price-(10.0/100*this.price);
+        return "Cena po obniżce 10% : "+result;
+    }
+    public String pricePlusDefaultValue(int value){
+        double result = this.price+value;
+        return "Cena po zwiekszeniu :"+result;
+    }
+
 }
